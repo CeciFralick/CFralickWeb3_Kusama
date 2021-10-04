329dfd7568e82568ed968c62b46051389562fbb5
CeciFralick <81652196+CeciFralick@users.noreply.github.com>
2021-10-04 16:06:45 -0400

    animations.added

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,5 +8,6 @@ gsap.timeline()
 .from("#hero", {duration: 1, alpha:0, opacity:0, yPercent:100})
 .from("#hero h1", {opacity:0, xPercent:100, duration:0.9})
 .from("#flower", {opacity:0, xPercent:100, duration:0.5})
-.from("#info img", {opacity:0, yPercent:100, duration:0.5})
+// .from("#info #line", {opacity:0, yPercent:100, duration:0.5})
+.from("#info #sun", {opacity:0, duration: 2, xPercent:100, rotation: "+=360"}, "spin")
 .from("#info p", {opacity:0, yPercent:100, duration:0.5});
