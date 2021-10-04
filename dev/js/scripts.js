9e86b53707b964afcaa360dfe29e36436c139c7f
CeciFralick <81652196+CeciFralick@users.noreply.github.com>
2021-10-04 10:24:11 -0400

    hw2

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,32 +2,9 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-// from = start point of timeline
-// mainTL.from("#hero article",{duration:2,alpha:0, scale:2});
-
-// to = end point of timeline
-// mainTL.to("#hero article",{duration:2,alpha:0, scale:2});
-
-const heroHeight = document.querySelector("#flower");
-
 const aniTime = 0.5;
 
-gsap.to("#flower", {x: 100, duration: 1});
-gsap.to("#flower", {y: 50, duration: 1, delay: 1});  
-gsap.to("#flower", {opacity: 0, duration: 1, delay: 2});
-
-mainTL.from("#flower",{duration:aniTime,alpha:0, y: heroHeight.clientHeight})
-gsap.timeline({ defaults: {duration: 1, ease: "elastic"} } )
-.to("#flower", {rotation: -270})
-  .to("#flower", {rotation: -360})
-  .to("#flower", {rotation: -180});
-
-mainTL.from("#flower",{duration:aniTime,alpha:0, y: heroHeight.clientHeight})
-    .from("#flower",{duration:aniTime, y:"+=300", alpha:0, rotation:180}, "madeUp");
-
-
-gsap.set("#flower",{scale:1});
 gsap.timeline()
-.from("#flower", {duration: 1, opacity:0})
+.from("#hero", {duration: 1, alpha:0, opacity:0})
 .from("#flower", {y: 50, duration: 1, delay: 1})
-.from("#flower", {opacity: 0, duration: 1, delay: 2});
+.from("#flower", {xPercent:100, duration:0.2});
