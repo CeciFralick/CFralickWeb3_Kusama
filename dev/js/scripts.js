e283700137ed7f305e3a0d2cd45524e71aa177df
CeciFralick <81652196+CeciFralick@users.noreply.github.com>
2021-11-15 15:02:24 -0500

    Start2

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,14 +1,12 @@
 import { gsap } from "gsap";
 
-const mainTL = gsap.timeline();
+const TL = gsap.timeline();
 
-const aniTime = 0.5;
 
 gsap.timeline()
-.from("#header img", {duration: 1, alpha:0, opacity:0, xPercent:-100})
-.from("#hero", {duration: 1, alpha:0, opacity:0, yPercent:100})
-.from("#hero h1", {opacity:0, xPercent:100, duration:0.9})
-.from("#flower", {opacity:0, xPercent:100, duration:0.5})
+.from("#smbody", {duration: 1, alpha:0, opacity:0, xPercent:-100})
+.from("#body", {duration: 1, alpha:0, opacity:0, yPercent:100})
+.from("#Lense", {opacity:0, xPercent:100, duration:0.9})
+.from("#smflash", {opacity:0, xPercent:100, duration:0.5})
 // .from("#info #line", {opacity:0, yPercent:100, duration:0.5})
-.from("#info #sun", {opacity:0, duration: 2, xPercent:100, rotation: "+=360"}, "spin")
-.from("#info p", {opacity:0, yPercent:100, duration:0.5});
+;
