c6eaea4effa24f3af00dbf516b871a7e00b49042
CeciFralick <81652196+CeciFralick@users.noreply.github.com>
2021-11-17 11:41:48 -0500

    lotsofchanges

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,20 +7,30 @@ gsap.registerPlugin(GSDevTools, MorphSVGPlugin, DrawSVGPlugin);
 
 gsap.set("#smbody",{transformOrigin: "50% 50%"})
 gsap.set("#lense",{transformOrigin: "50% 50%"})
+gsap.set("#tleft",{transformOrigin: "bottom left"})
+gsap.set("#tright",{transformOrigin: "top left"})
 
 const mainTL = gsap.timeline();
 // mainTL.from("tleft",{duration:0.25, scaleY:0, ease: "none)"},"-=20%")
-  mainTL.from("tleft",{drawSVG:"10% 100%", ease:"none"})
+mainTL.from("#tleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "lsame")
+mainTL.from("#bleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "lsame")
+mainTL.from("#bright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.2}, "lsame")
+mainTL.from("#tright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.2}, "lsame")
+mainTL.to("#tleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
+mainTL.to("#bleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
+mainTL.to("#bright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
+mainTL.to("#tright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
+
 mainTL.from("#smbody",{opacity:0, duration:1, y:"1000", rotate:-180})
-mainTL.to("#smbody", {morphSVG:"#body",duration:.4})
-mainTL.from("#smflash",{duration:1},"playSameTime")
-mainTL.to("#smflash", {morphSVG:"#flash",duration:.4},"playSameTime")
+mainTL.to("#smbody", {morphSVG:"#body",duration: .5, ease: "back.out(1.7)"})
+mainTL.from("#smflash",{duration:0},"playSameTime")
+mainTL.to("#smflash", {morphSVG:"#flash",duration: .5, ease: "back.out(1.7)"})
 mainTL.from("#lense",{scale:0},"same")
 mainTL.to("#lense",{xPercent:40})
 mainTL.to("#lense",{xPercent:0})
-mainTL.from("#top",{yPercent:40, autoAlpha:0},"same")
-mainTL.from("#bbase",{yPercent:40, autoAlpha:0},"same")
-mainTL.from("#button",{yPercent:40, autoAlpha:0},"same")
+mainTL.from("#top",{yPercent:40, autoAlpha:0, duration: .5, ease: "back.out(1.7)"},"same")
+mainTL.from("#bbase",{yPercent:40, autoAlpha:0,duration: .5, ease: "back.out(1.7)"},"same")
+mainTL.from("#button",{yPercent:40, autoAlpha:0,duration: .5, ease: "back.out(1.9)"},"same" )
 
 
 ;
@@ -50,8 +60,8 @@ mainTL.from("#button",{yPercent:40, autoAlpha:0},"same")
 
 
 GSDevTools.create();
-
-
+DrawSVGPlugin.create();
+MorphSVGPlugin.create();
 
 
 
