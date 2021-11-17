c3ab434b6e669c93fe72c0ea3b12b318282a673b
CeciFralick <81652196+CeciFralick@users.noreply.github.com>
2021-11-17 12:25:27 -0500

    evenmorefixes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,58 +10,84 @@ gsap.set("#lense",{transformOrigin: "50% 50%"})
 gsap.set("#tleft",{transformOrigin: "bottom left"})
 gsap.set("#tright",{transformOrigin: "top left"})
 
-const mainTL = gsap.timeline();
-// mainTL.from("tleft",{duration:0.25, scaleY:0, ease: "none)"},"-=20%")
+const mainTL = gsap.timeline()
+
 mainTL.from("#tleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "lsame")
-mainTL.from("#bleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "lsame")
-mainTL.from("#bright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.2}, "lsame")
-mainTL.from("#tright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.2}, "lsame")
-mainTL.to("#tleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
-mainTL.to("#bleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
-mainTL.to("#bright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
-mainTL.to("#tright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
-
-mainTL.from("#smbody",{opacity:0, duration:1, y:"1000", rotate:-180})
-mainTL.to("#smbody", {morphSVG:"#body",duration: .5, ease: "back.out(1.7)"})
-mainTL.from("#smflash",{duration:0},"playSameTime")
-mainTL.to("#smflash", {morphSVG:"#flash",duration: .5, ease: "back.out(1.7)"})
-mainTL.from("#lense",{scale:0},"same")
-mainTL.to("#lense",{xPercent:40})
-mainTL.to("#lense",{xPercent:0})
-mainTL.from("#top",{yPercent:40, autoAlpha:0, duration: .5, ease: "back.out(1.7)"},"same")
-mainTL.from("#bbase",{yPercent:40, autoAlpha:0,duration: .5, ease: "back.out(1.7)"},"same")
-mainTL.from("#button",{yPercent:40, autoAlpha:0,duration: .5, ease: "back.out(1.9)"},"same" )
-
-
-function lines(){
-    const tl=gsap.timeline();
-
-    return tl;
-}
+.from("#bleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "lsame")
+.from("#bright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.2}, "lsame")
+.from("#tright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.2}, "lsame")
+.to("#tleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
+.to("#bleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
+.to("#bright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
+.to("#tright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
+.from("#smbody",{opacity:0, duration:1, y:"1000", rotate:-180})
+.to("#smbody", {morphSVG:"#body",duration: .5, ease: "back.out(1.7)"})
+.from("#smflash",{duration:0},"playSameTime")
+.to("#smflash", {morphSVG:"#flash",duration: .5, ease: "back.out(1.7)"}) 
+.from("#lense",{scale:0},"same")
+.to("#lense",{xPercent:40})
+.to("#lense",{xPercent:0})
+.from("#top",{yPercent:40, autoAlpha:0, duration: .5, ease: "back.out(1.7)"},"same")
+.from("#bbase",{yPercent:40, autoAlpha:0,duration: .5, ease: "back.out(1.7)"},"same")
+.from("#button",{yPercent:40, autoAlpha:0,duration: .5, ease: "back.out(1.9)"},"same" )
 
 
-;
+// function lines(){
+//     const tl=gsap.timeline();
+//     tl.from("#tleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "lsame")
+//     tl.from("#bleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "lsame")
+//     tl.from("#bright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.2}, "lsame")
+//     tl.from("#tright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.2}, "lsame")
+//     tl.to("#tleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
+//     tl.to("#bleft",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
+//     tl.to("#bright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
+//     tl.to("#tright",{drawSVG: 0, ease:"none", duration:.5, stagger: 0.1}, "osame")
+//     return tl;
+// }
+
+// function cameraappears(){
+//     const tl=gsap.timeline();
+//     tl.from("#smbody",{opacity:0, duration:1, y:"1000", rotate:-180})
+//     tl.to("#smbody", {morphSVG:"#body",duration: .5, ease: "back.out(1.7)"})
+//     tl.from("#smflash",{duration:0},"playSameTime")
+//     tl.to("#smflash", {morphSVG:"#flash",duration: .5, ease: "back.out(1.7)"}) 
+//     return tl;
+// }
 
 
-// mainTL.add(fivedots())
-// .add(layertwo())
-// .add(layerthree())
-// .add(layerfour())
-// .add(layerfive())
-// .add(layersix())
-// .add(layerseven())
-// // .add(layereight())
-// .add(layernine(),"-=99%")
-// .add(layerten())
-// .add(layereleven())
-// .add(layertwelve())
-// .add(layerthirteen())
-// .add(layerfourteen(),"playSameTime")
-// .add(layerfifteen())
-// .add(layerdots(),"playSameTime")
-// .add(endseq())
-// .add(heroanimations(),"-=25%")
-// ;
+// function cameraextras(){
+//     const tl=gsap.timeline();
+//     tl.from("#lense",{scale:0},"same")
+//     tl.to("#lense",{xPercent:40})
+//     tl.to("#lense",{xPercent:0})
+//     tl.from("#top",{yPercent:40, autoAlpha:0, duration: .5, ease: "back.out(1.7)"},"same")
+//     tl.from("#bbase",{yPercent:40, autoAlpha:0,duration: .5, ease: "back.out(1.7)"},"same")
+//     tl.from("#button",{yPercent:40, autoAlpha:0,duration: .5, ease: "back.out(1.9)"},"same" )
+//     return tl;
+// }
+
+
+
+
+// // mainTL.add(fivedots())
+// // .add(layertwo())
+// // .add(layerthree())
+// // .add(layerfour())
+// // .add(layerfive())
+// // .add(layersix())
+// // .add(layerseven())
+// // // .add(layereight())
+// // .add(layernine(),"-=99%")
+// // .add(layerten())
+// // .add(layereleven())
+// // .add(layertwelve())
+// // .add(layerthirteen())
+// // .add(layerfourteen(),"playSameTime")
+// // .add(layerfifteen())
+// // .add(layerdots(),"playSameTime")
+// // .add(endseq())
+// // .add(heroanimations(),"-=25%")
+// // ;
 
 
 
@@ -73,16 +99,14 @@ MorphSVGPlugin.create();
 
 
 
-// const aniTime = 0.5;
+// // const aniTime = 0.5;
 
-<<<<<<< Updated upstream
-mainTL.add(lines())
-.add(layertwo())
-
-
+// mainTL.add(lines())
+// .add(cameraappears())
+// .add(cameraextras())
 
 
-;
-=======
-gsap.timeline()
->>>>>>> Stashed changes
+
+
+// ;
+// gsap.timeline()
