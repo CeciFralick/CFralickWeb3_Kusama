182c683aeab12d5bd6e20741dc3dc03e11157f2d
CeciFralick <81652196+CeciFralick@users.noreply.github.com>
2021-11-22 14:40:52 -0500

    goodstartiguess

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,10 +5,10 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin(GSDevTools, MorphSVGPlugin, DrawSVGPlugin);
 
-gsap.set("#lense",{transformOrigin: "50% 50%"})
-gsap.set("#smflash",{transformOrigin: "50% 50%"})
-gsap.set("#tleft",{transformOrigin: "bottom left"})
-gsap.set("#tright",{transformOrigin: "top left"})
+// gsap.set("#lense",{transformOrigin: "50% 50%"})
+// gsap.set("#smflash",{transformOrigin: "50% 50%"})
+// gsap.set("#tleft",{transformOrigin: "bottom left"})
+// gsap.set("#tright",{transformOrigin: "top left"})
 
 const mainTL = gsap.timeline()
 
@@ -31,65 +31,72 @@ const mainTL = gsap.timeline()
 // .from("#bbase",{yPercent:40, autoAlpha:0,duration: .5, ease: "back.out(1.7)"},"same")
 // .from("#button",{yPercent:40, autoAlpha:0,duration: .5, ease: "back.out(1.9)"},"same" )
 
+// {drawSVG: 0, ease:"none", duration:.2, stagger: 0.1}, "lsame")
 
-function lines(){
+function begins(){
     const tl=gsap.timeline();
-    tl.from("#tleft",{drawSVG: 0, ease:"none", duration:.2, stagger: 0.1}, "lsame")
-    tl.from("#bleft",{drawSVG: 0, ease:"none", duration:.2, stagger: 0.1}, "lsame")
-    tl.from("#bright",{drawSVG: 0, ease:"none", duration:.2, stagger: 0.2}, "lsame")
-    tl.from("#tright",{drawSVG: 0, ease:"none", duration:.2, stagger: 0.2}, "lsame")
-    tl.to("#tleft",{drawSVG: 0, ease:"none", duration:.4, stagger: 0.5}, "osame")
-    tl.to("#bleft",{drawSVG: 0, ease:"none", duration:.4, stagger: 0.5}, "osame")
-    tl.to("#bright",{drawSVG: 0, ease:"none", duration:.2, stagger: 0.1}, "osame")
-    tl.to("#tright",{drawSVG: 0, ease:"none", duration:.2, stagger: 0.1}, "osame")
-    
-    tl.to("#tleft",{morphSVG:"#smtleft",duration: .5, ease: "back.out(1.7)"}, "tsame")
-    tl.to("#bleft",{morphSVG:"#smbleft",duration: .5, ease: "back.out(1.7)"}, "tsame")
-    tl.to("#bright",{morphSVG:"#smbright",duration: .5, ease: "back.out(1.7)"}, "tsame")
-    tl.to("#tright",{morphSVG:"#smtright",duration: .5, ease: "back.out(1.7)"}, "tsame")
-
+    tl.from("#outer",{yPercent:40, autoAlpha:0, duration: 2, ease: "elastic.out(1, 0.2)"})
+    tl.from("#mid",{yPercent:40, autoAlpha:0,duration: 2, ease: "elastic.out(1, 0.3)"},"-=99%")
+    tl.from("#middle",{yPercent:40, autoAlpha:0,duration: 2, ease: "elastic.out(1, 0.3)"},"-=70%")
+    // tl.from("#Hat",{drawSVG: 0, ease:"none", duration:.2, stagger: 0.1})
+    tl.to("#mid", {morphSVG:"#HatBand",duration: .5, ease: "back.out(1.7)"})
+    tl.to("#outer", {morphSVG:"#LeftSide",duration: .5, ease: "back.out(1)"})
+    tl.to("#LeftSide", {morphSVG:"#RightSide",duration: .5, ease: "back.out(1)"},"-=50%")
+    tl.to("#mid", {morphSVG:"#HatBand",duration: .5, ease: "back.out(1.7)"})
+    tl.to("#middle",{morphSVG:"#LeftChunk",duration: .5, ease: "back.out(1)"})
+    tl.to("#LeftChunk",{morphSVG:"#RightChunk",duration: .5, ease: "back.out(1)"})
+    // tl.from("#Whte",{yPercent:40, autoAlpha:0,duration: 1.5, ease: "elastic.out(2.75, 0.4)"}, "same")
     // tl.to("#tleft",{drawSVG:0, ease:"none", duration:.4, stagger: 0.5},"ttime")
     // tl.to("#bleft",{drawSVG: 0 , ease:"none", duration:.4, stagger: 0.5}, "ttime")
     return tl;
 }
 
-function cameraappears(){
+function wholeface(){
     const tl=gsap.timeline();
-    tl.from("#smbody",{opacity:0, duration:1, y:"1000", rotate:-180})
-    tl.to("#smbody", {morphSVG:"#body",duration: .5, ease: "back.out(1.7)"})
+    tl.from("#Face",{drawSVG: 0, ease:"none", duration:1, stagger: 0.1})
+    tl.from("#Vector1", {drawSVG: 0, ease:"none", duration:.5, stagger: 0.1})
+    tl.from("#Vector2", {drawSVG: 0, ease:"none", duration:.5, stagger: 0.1})
+    tl.from("#Vector3", {drawSVG: 0, ease:"none", duration:.5, stagger: 0.1})
+    tl.from("#Vector4", {drawSVG: 0, ease:"none", duration:.5, stagger: 0.1})
+    tl.from("#Vector5", {drawSVG: 0, ease:"none", duration:.5, stagger: 0.1})
+    tl.from("#Vector6", {drawSVG: 0, ease:"none", duration:.5, stagger: 0.1})
+    tl.from("#Left.Brow", {ease:"none", duration:2, stagger: 0.1})
+    tl.from("#Right.Brow", {ease:"none", duration:2, stagger: 0.1})
     return tl;
 }
 
-function flashes(){
+function hairstuff(){
     const tl=gsap.timeline();
-    tl.from("#smflash",{duration:0},"playSameTime")
-    tl.to("#smflash", {morphSVG:"#flash",duration: .5, ease: "back.out(1.7)"}) 
+    tl.from("Left.Out",{duration:1, opacity: 1},"playSameTime")
+    tl.from("Left.Mid",{duration:1, opacity: 1},"playSameTime")
+    tl.from("Left.Cheek",{duration:1, opacity: 1},"playSameTime")
+    tl.from("Left.Bang",{duration:1, opacity: 1},"playSameTime")
     return tl;
 }
 
-function lenses(){
-    const tl=gsap.timeline();
-    tl.from("#lense",{scale:0, duration: .4})
-    tl.to("#lense",{xPercent:40, duration: .4})
-    tl.to("#lense",{xPercent:0, duration: .4})
-    return tl;
-}
+// function lenses(){
+//     const tl=gsap.timeline();
+//     tl.from("#lense",{scale:0, duration: .4})
+//     tl.to("#lense",{xPercent:40, duration: .4})
+//     tl.to("#lense",{xPercent:0, duration: .4})
+//     return tl;
+// }
 
-function cameratop(){
-    const tl=gsap.timeline();
-    tl.from("#top",{yPercent:40, autoAlpha:0, duration: 2, ease: "elastic.out(1.75, 0.2)"},"same")
-    tl.from("#bbase",{yPercent:40, autoAlpha:0,duration: 2, ease: "elastic.out(1.5, 0.3)"},"same")
-    tl.from("#button",{yPercent:40, autoAlpha:0,duration: 1.5, ease: "elastic.out(2.75, 0.4)"}, "same")
-    return tl;
-}
+// function cameratop(){
+//     const tl=gsap.timeline();
+//     tl.from("#top",{yPercent:40, autoAlpha:0, duration: 2, ease: "elastic.out(1.75, 0.2)"},"same")
+//     tl.from("#bbase",{yPercent:40, autoAlpha:0,duration: 2, ease: "elastic.out(1.5, 0.3)"},"same")
+//     tl.from("#button",{yPercent:40, autoAlpha:0,duration: 1.5, ease: "elastic.out(2.75, 0.4)"}, "same")
+//     return tl;
+// }
 
 
 
-mainTL.add(lines())
-.add(cameraappears(),"-=99%")
-.add(flashes(),"-=60%")
-.add(lenses(),"sameTime", "-=60%")
-.add(cameratop(),"sameTime")
+mainTL.add(begins())
+.add(wholeface())
+.add(hairstuff())
+// .add(lenses(),"sameTime", "-=60%")
+// .add(cameratop(),"sameTime")
 ;
 
 // // mainTL.add(fivedots())
@@ -122,11 +129,11 @@ MorphSVGPlugin.create();
 
 
 
-// // const aniTime = 0.5;
+// const aniTime = 0.5;
 
 
 
 
 
 // ;
-// gsap.timeline()
+gsap.timeline()
